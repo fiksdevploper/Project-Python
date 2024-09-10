6673d50671357e4e384998bd6cc6d18f538cfb1f
Muhammad Fikri <muhammadfikri10050@gmail.com>
2024-09-10 22:45:26 +0700

    update

--- day 15/algoritma.py
+++
@@ -1,9 +0,0 @@
-print ("Program Python Persegi Panjang Bintang")
-print ("======================================")
-print ()
-
-diagonal1 = float(input('input panjang diagonal 1: '))
-diagonal2 = float(input('input panjang diagonal 2: '))
-
-luas = 0.5 * diagonal1 * diagonal2
-print ('luasnya bela ketupat =',round(luas,2))
